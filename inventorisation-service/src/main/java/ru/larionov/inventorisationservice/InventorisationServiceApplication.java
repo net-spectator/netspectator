d52c81c5858e87a09ed7eaafe153b05003ee34a8
Boris Larionov <79999987388@ya.ru>
2023-06-09 17:41:08 +0300

    create modules

---
+++ inventorisation-service/src/main/java/ru/larionov/inventorisationservice/InventorisationServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.larionov.inventorisationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorisationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorisationServiceApplication.class, args);
+    }
+
+}
---
+++ inventorisation-service/src/test/java/ru/larionov/inventorisationservice/InventorisationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.larionov.inventorisationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorisationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
