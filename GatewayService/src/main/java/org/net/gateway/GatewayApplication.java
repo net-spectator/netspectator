3e58dda1a8ea17071d2a94a00e3266d01d554142
W0iD <xcrypter@gmail.com>
2023-06-12 02:23:04 +0300

    Feature-NS-s1c9 - "Прозрачная" валидация токена. Проверяется наличие заголовка Authorization, проверяется токен по public/private key, проверяется дата истечения токена. - В пересылаемый ответ добавляется заголовок со значением поля, извлекаемого из принимаемого токена. Поле настраивается в конфиге в директиве: "introspection_field".

---
+++ GatewayService/src/main/java/org/net/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package org.net.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@SpringBootApplication
+@EnableWebFluxSecurity
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
