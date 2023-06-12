5de2807ea6c2c05bef290b6b271e40dc6a90eb94
GladiatorMPVZ <inform.work981@mail.ru>
2023-06-12 15:58:55 +0300

    Creating Entities for a Device Service

--- WebDeviceService/src/main/java/org/net/webcoreservice/WebDeviceServiceApplication.java
+++ WebDeviceService/src/main/java/org/net/webcoreservice/WebDeviceServiceApplication.java
@@ -3,13 +3,11 @@ package org.net.webcoreservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLException;
-
 @SpringBootApplication
 public class WebDeviceServiceApplication {
 
     public static void main(String[] args) {
-        tcpServerStart();
+//        tcpServerStart();
         SpringApplication.run(WebDeviceServiceApplication.class, args);
     }
 
@@ -17,11 +15,11 @@ public class WebDeviceServiceApplication {
      стартует TCP сервер для H2 (при использовании БД отключить),
      необходим для совместного использования базы данных между DirServer и Web
     */
-    private static void tcpServerStart(){
-        try {
-            org.h2.tools.Server server = org.h2.tools.Server.createTcpServer().start();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private static void tcpServerStart(){
+//        try {
+//            org.h2.tools.Server server = org.h2.tools.Server.createTcpServer().start();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
