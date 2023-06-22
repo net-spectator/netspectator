0836d66b45c0941515343bd38a5699cf3cae2cbe
Sergey Klementev <73162149+GladiatorMPVZ@users.noreply.github.com>
2023-06-23 00:09:03 +0300

    add operations with blackList and hardwareInfo

--- WebDeviceService/src/main/java/org/net/webcoreservice/controllers/TrackedEquipmentController.java
+++ WebDeviceService/src/main/java/org/net/webcoreservice/controllers/TrackedEquipmentController.java
@@ -1,11 +1,13 @@
 package org.net.webcoreservice.controllers;
 
+import entities.devices.ClientHardwareInfo;
 import lombok.RequiredArgsConstructor;
 import org.net.webcoreservice.converters.TrackedEquipmentConverter;
 import org.net.webcoreservice.dto.TrackedEquipmentDto;
 import org.net.webcoreservice.exeptions.ResourceNotFoundException;
 import org.net.webcoreservice.service.TrackedEquipmentService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,35 @@ public class TrackedEquipmentController {
     public List<TrackedEquipmentDto> getAllEquipment() {
         return trackedEquipmentService.findAll().stream().map(converter::entityToDto).collect(Collectors.toList());
     }
+
+    @GetMapping("/hardwareInfo/{id}")
+    public ClientHardwareInfo getHardwareInfo(@PathVariable Long id) {
+        return trackedEquipmentService.getEquipmentHardwareInfo(id);
+    }
+
+    @GetMapping("/blackList")
+    public List<TrackedEquipmentDto> getAllBlackList() {
+        return trackedEquipmentService.showBlackList().stream().map(converter::entityToDto).collect(Collectors.toList());
+    }
+
+    @PutMapping("/addBlackList/{id}")
+    public ResponseEntity<?> addClientToBlackList(@PathVariable Long id) {
+        if (!trackedEquipmentService.isExist(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        trackedEquipmentService.addToBlackList(id);
+        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/removeBlackList/{id}")
+    public ResponseEntity<?> removeClientToBlackList(@PathVariable Long id) {
+        if (!trackedEquipmentService.isExist(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        trackedEquipmentService.removeFromBlackList(id);
+        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+    }
+
 
     @GetMapping("/{id}")
     public TrackedEquipmentDto getEquipmentById(@PathVariable Long id) {
