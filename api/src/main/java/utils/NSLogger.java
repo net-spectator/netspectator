68ca7c55bb9153737a78b6ab1a67d369e075e5c0
alekseyandreev <alexav5@yandex.ru>
2023-06-28 00:13:20 +0700

    add loggers to sensors

--- api/src/main/java/utils/NSLogger.java
+++ api/src/main/java/utils/NSLogger.java
@@ -4,8 +4,16 @@ import org.apache.log4j.Logger;
 
 public class NSLogger {
     private Logger logger;
-
+    private boolean disabled; //позволяет отключать логирование событий (можно будет потом убрать если не нужно)
     private final Class<?> clazz;
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
 
     public NSLogger(Class<?> clazz) {
         this.clazz = clazz;
@@ -13,23 +21,32 @@ public class NSLogger {
     }
 
     private void init() {
+        disabled = false;
         logger = Logger.getLogger(clazz.getName());
     }
 
     public void info(String message) {
-        logger.info(message);
+        if (!disabled) {
+            logger.info(message);
+        }
     }
 
     public void info(String message, Throwable t) {
-        logger.info(message, t);
+        if (!disabled) {
+            logger.info(message, t);
+        }
     }
 
     public void error(String message) {
-        logger.error(message);
+        if (!disabled) {
+            logger.error(message);
+        }
     }
 
     public void error(String message, Throwable t) {
-        logger.error(message, t);
+        if (!disabled) {
+            logger.error(message, t);
+        }
     }
 
 }
